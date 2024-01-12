6552798e87003d8f9cd77d6f2ce8927b13f5363c
Nikita Zasimuk <nikita.zasimuk@protofire.io>
2024-01-12 21:11:32 +0300

    refactor: update footer

--- src/components/common/Footer/index.tsx
+++ src/components/common/Footer/index.tsx
@@ -6,10 +6,10 @@ import { useRouter } from 'next/router'
 import css from './styles.module.css'
 import { AppRoutes } from '@/config/routes'
 import packageJson from '../../../../package.json'
-import AppstoreButton from '../AppStoreButton'
+// import AppstoreButton from '../AppStoreButton'
 import ExternalLink from '../ExternalLink'
 import MUILink from '@mui/material/Link'
-import { HELP_CENTER_URL, IS_DEV, IS_OFFICIAL_HOST } from '@/config/constants'
+import { HELP_CENTER_URL, /* IS_DEV,*/ IS_OFFICIAL_HOST } from '@/config/constants'
 
 const footerPages = [
   AppRoutes.welcome.index,
@@ -45,7 +45,7 @@ const Footer = (): ReactElement | null => {
   return (
     <footer className={css.container}>
       <ul>
-        {IS_OFFICIAL_HOST || IS_DEV ? (
+        {IS_OFFICIAL_HOST /*|| IS_DEV*/ ? (
           <>
             <li>
               <Typography variant="caption">&copy;2022–{new Date().getFullYear()} Core Contributors GmbH</Typography>
@@ -74,18 +74,16 @@ const Footer = (): ReactElement | null => {
               </ExternalLink>
             </li>
           </>
-        ) : (
-          <li>{'This is an unofficial distribution of Safe{Wallet}'}</li>
-        )}
+        ) : null}
 
         <li>
           <ExternalLink href={`${packageJson.homepage}/releases/tag/v${packageJson.version}`} noIcon>
             <SvgIcon component={GitHubIcon} inheritViewBox fontSize="inherit" sx={{ mr: 0.5 }} /> v{packageJson.version}
           </ExternalLink>
         </li>
-        <li>
+        {/*<li>
           <AppstoreButton placement="footer" />
-        </li>
+        </li>*/}
       </ul>
     </footer>
   )
